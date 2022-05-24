5db08fb1fbb064f2e7b5a131b02f32a40b6e4c23
SoNgSeUNgMiN96 <tmd9544@gmail.com>
2022-05-25 02:10:50 +0900

    시뮬레이션 2048 업데이트
    시뮬레이션 알고리즘으로 최대점수인 11064 달성
    현재 depth 8
    
    작성자: 송승민
    작성일 : 2022-05-25

--- main.py
+++ main.py
@@ -21,6 +21,9 @@ clock = pygame.time.Clock()
 mainscore = 0
 simulScore = 0
 minDepth = 0
+maxSimul = 0
+code = 0
+minTwoandFour = 20
 
 
 
@@ -150,65 +153,306 @@ def move(map, direc, score, mode= False):
 
     return success
 
-#처음엔 마냥 시뮬레이션으로 최댓값이 오게 설정을 하게되었는데 depth에 따라 과 시뮬레이션으로 모두 만점을 받을 수 있음.
+
+
+#direc 0 = left, 1 = right 2 = up 3 = down
+def move2(map, direc, score, mode= False):
+    success = False
+    global mainscore
+    global simulScore
+
+    if direc == 0:
+        for i in range(4):
+            for j in range(1, 4):
+                for k in range(j, 0, -1):
+                    if map[i][k-1] == 0 and map[i][k] !=0:
+                        map[i][k-1] = map[i][k]
+                        map[i][k] = 0
+                        success = True
+
+        for i in range(4):
+            for j in range(1, 4):
+                if map[i][j-1] == map[i][j] and map[i][j-1] != 0:
+                    map[i][j - 1] += map[i][j]
+                    map[i][j] = 0
+
+                    simulScore = simulScore +1
+
+                    success = True
+
+        for i in range(4):
+            for j in range(1, 4):
+                for k in range(j, 0, -1):
+                    if map[i][k-1] == 0 and map[i][k] !=0:
+                        map[i][k - 1] = map[i][k]
+                        map[i][k] = 0
+
+    if direc == 1:
+        for i in range(4):
+            for j in range(3, -1, -1):
+                for k in range(j, 3):
+                    if map[i][k+1] == 0 and map[i][k] != 0:
+                        map[i][k+1] = map[i][k]
+                        map[i][k] = 0
+                        success = True
+
+        for i in range(4):
+            for j in range(3, 0, -1):
+                if map[i][j-1] == map[i][j] and map[i][j] != 0:
+                    map[i][j] += map[i][j - 1]
+                    map[i][j - 1] = 0
+                    success = True
+                    simulScore = simulScore +1
+
+
+        for i in range(4):
+            for j in range(3, -1, -1):
+                for k in range(j, 3):
+                    if map[i][k + 1] == 0 and map[i][k] != 0:
+                        map[i][k + 1] = map[i][k]
+                        map[i][k] = 0
+
+    if direc == 2:
+        for i in range(4):
+            for j in range(1, 4):
+                for k in range(j, 0, -1):
+                    if map[k-1][i] == 0 and map[k][i] != 0:
+                        map[k-1][i] = map[k][i]
+                        map[k][i] = 0
+                        success = True
+
+        for i in range(4):
+            for j in range(1, 4):
+                if map[j-1][i] == map[j][i] and map[j][i] != 0:
+                    map[j - 1][i] += map[j][i]
+                    map[j][i] = 0
+                    success = True
+
+                    simulScore = simulScore +1
+        for i in range(4):
+            for j in range(1, 4):
+                for k in range(j, 0, -1):
+                    if map[k-1][i] == 0 and map[k][i] != 0:
+                        map[k - 1][i] = map[k][i]
+                        map[k][i] = 0
+
+    if direc == 3:
+        for i in range(4):
+            for j in range(3, -1, -1):
+                for k in range(j, 3):
+                    if map[k+1][i] == 0 and map[k][i] != 0:
+                        map[k + 1][i] = map[k][i]
+                        map[k][i] = 0
+                        success = True
+
+        for i in range(4):
+            for j in range(3, 0, -1):
+                if map[j - 1][i] == map[j][i] and map[j][i] != 0:
+                    map[j][i] += map[j - 1][i]
+                    map[j - 1][i] = 0
+                    success = True
+                    simulScore = simulScore +1
+
+        for i in range(4):
+            for j in range(3, -1, -1):
+                for k in range(j, 3):
+                    if map[k+1][i] == 0 and map[k][i] != 0:
+                        map[k + 1][i] = map[k][i]
+                        map[k][i] = 0
+
+    return success
+
+
+#처음엔 시뮬레이션으로 최댓값이 오게 설정을 하게되었는데 depth에 따라 과 시뮬레이션으로 모두 동점을 받을 수있음
 #그렇다고 depth 임계치를 낮추면 정확도가 적거나, 최적해를 반영하기 힘듬.
 # 따라서 시뮬레이션에서 최댓값 + 그 최댓값의 최소 깊이를 반영해야 함.
+# 첫 번째 시뮬레이션은 지금 당장 높은 점수를 얻는 방향으로 진행됨.  두번째 시뮬레이션은 빈칸을 늘리는 방향으로 진행할 예정
 
+def simulation(list_map, depth, score, direc, orin):
+    global simulScore, maxSimul, minDepth
+    global code
 
-def simulation(list_map, depth, score, direc):
-    global simulScore
-    global minDepth
     simulScore = score
+    direcList =[0,1,2,3]
+    random.shuffle(direcList)
 
-    max = score
-    code = random.randrange(0, 4)
-
-    if depth == 2:
+    if depth == 8:
         return
 
-    if direc != 0:
+    if direc != direcList[0]:
+        if direc ==5:
+            orin = direcList[0]
         newMap = copy.deepcopy(list_map)
-        move(newMap, 0, simulScore)
-        simulation(newMap, depth + 1, simulScore, 0)
-        if max < simulScore:
-            max = simulScore
-            code = 0
+
+        move(newMap, direcList[0], simulScore)
+        if maxSimul < simulScore:
+            maxSimul = simulScore
+            minDepth = depth
+            code = orin
+        elif maxSimul == simulScore:
+            if minDepth > depth:
+                maxSimul = simulScore
+                minDepth = depth
+                code = orin
+        simulation(newMap, depth + 1, simulScore, direcList[0], orin)
+
         simulScore = score
 
-    if direc != 1:
+    if direc != direcList[1]:
+        if direc ==5:
+            orin = direcList[1]
         newMap = copy.deepcopy(list_map)
-        move(newMap, 1, simulScore)
-        simulation(newMap, depth + 1, simulScore, 1)
-        if max < simulScore:
-            max = simulScore
-            code = 1
+        move(newMap, direcList[1], simulScore)
+        if maxSimul < simulScore:
+            maxSimul = simulScore
+            minDepth = depth
+            code = orin
+        elif maxSimul == simulScore:
+            if minDepth > depth:
+                maxSimul = simulScore
+                minDepth = depth
+                code = orin
+        simulation(newMap, depth + 1, simulScore, direcList[1], orin)
+
+
         simulScore = score
 
-    if direc != 2:
+    if direc != direcList[2]:
+        if direc ==5:
+            orin = direcList[2]
         newMap = copy.deepcopy(list_map)
-        move(newMap, 2, simulScore)
-        simulation(newMap, depth + 1, simulScore, 2)
-        if max < simulScore:
-            max = simulScore
-            code = 2
+        move(newMap, direcList[2], simulScore)
+        if maxSimul < simulScore:
+            maxSimul = simulScore
+            minDepth = depth
+            code = orin
+        elif maxSimul == simulScore:
+            if minDepth > depth:
+                maxSimul = simulScore
+                minDepth = depth
+                code = orin
+        simulation(newMap, depth + 1, simulScore, direcList[2], orin)
         simulScore = score
 
-    if direc != 3:
+    if direc != direcList[3]:
+        if direc ==5:
+            orin = direcList[3]
         newMap = copy.deepcopy(list_map)
-        move(newMap, 3, simulScore)
-        simulation(newMap, depth + 1, simulScore, 3)
-        if max < simulScore:
-            max = simulScore
-            code = 3
+        move(newMap, direcList[3], simulScore)
+        if maxSimul < simulScore:
+            maxSimul = simulScore
+            minDepth = depth
+            code = orin
+        elif maxSimul == simulScore:
+            if minDepth > depth:
+                maxSimul = simulScore
+                minDepth = depth
+                code = orin
+        simulation(newMap, depth + 1, simulScore, direcList[3], orin)
         simulScore = score
 
     return code
 
 
 
+#공평하게 갯수를 가장 많이 줄이는것에 초점을 둬본다.
+def simulation2(list_map, depth, score, direc, orin):
+    global simulScore, maxSimul, minDepth
+    global code
+
+    simulScore = score
+    direcList =[0,1,2,3]
+    random.shuffle(direcList)       #순서비교의 편향을 방지
+
+    if depth == 8:
+        return
+
+    if direc != direcList[0]:
+        if direc ==5:
+            orin = direcList[0]
+        newMap = copy.deepcopy(list_map)
+
+        move2(newMap, direcList[0], simulScore)
+        if maxSimul < simulScore:
+            maxSimul = simulScore
+            minDepth = depth
+            code = orin
+        elif maxSimul == simulScore:
+            if minDepth > depth:
+                maxSimul = simulScore
+                minDepth = depth
+                code = orin
+        simulation2(newMap, depth + 1, simulScore, direcList[0], orin)
+
+        simulScore = score
+
+    if direc != direcList[1]:
+        if direc ==5:
+            orin = direcList[1]
+        newMap = copy.deepcopy(list_map)
+        move2(newMap, direcList[1], simulScore)
+        if maxSimul < simulScore:
+            maxSimul = simulScore
+            minDepth = depth
+            code = orin
+        elif maxSimul == simulScore:
+            if minDepth > depth:
+                maxSimul = simulScore
+                minDepth = depth
+                code = orin
+        simulation2(newMap, depth + 1, simulScore, direcList[1], orin)
+
+
+        simulScore = score
+
+    if direc != direcList[2]:
+        if direc ==5:
+            orin = direcList[2]
+        newMap = copy.deepcopy(list_map)
+        move2(newMap, direcList[2], simulScore)
+        if maxSimul < simulScore:
+            maxSimul = simulScore
+            minDepth = depth
+            code = orin
+        elif maxSimul == simulScore:
+            if minDepth > depth:
+                maxSimul = simulScore
+                minDepth = depth
+                code = orin
+        simulation2(newMap, depth + 1, simulScore, direcList[2], orin)
+        simulScore = score
+
+    if direc != direcList[3]:
+        if direc ==5:
+            orin = direcList[3]
+        newMap = copy.deepcopy(list_map)
+        move2(newMap, direcList[3], simulScore)
+        if maxSimul < simulScore:
+            maxSimul = simulScore
+            minDepth = depth
+            code = orin
+        elif maxSimul == simulScore:
+            if minDepth > depth:
+                maxSimul = simulScore
+                minDepth = depth
+                code = orin
+        simulation2(newMap, depth + 1, simulScore, direcList[3], orin)
+        simulScore = score
+
+
+    return code
+
+def countTwoFour(list_map):
+    count = 0
+    for i in range(4):
+        for j in range(4):
+            if list_map[i][j] == 2 or list_map[i][j] == 4:
+                count = count +1
+    return count
+
+
 def runGame():
-    global done
-    global gameOver
+    global done, gameOver, maxSimul, minDepth, code
 
     list_map = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
     null_list = []
@@ -314,10 +558,20 @@ def runGame():
                         break
 
                 elif event.key == pygame.K_SPACE:
-                    code = 0
-                    code = simulation(list_map, 0, mainscore,  5)
+                    code = random.randrange(0,4)
+                    maxSimul = 0
+                    minDepth = 100
+                    minTwoandFour =20
+                    #simulScore = 0
+                    simulScore = mainscore
+
+                    simulation(list_map, 0, mainscore,  5, 5)
+
+                    #(list_map, 0, 0, 5, 5)
                     if not move(list_map, code, mainscore, True):
-                        break
+                        code = random.randrange(0,4)
+                        if not move(list_map, code, mainscore, True):
+                            break
 
                 null_list = []
                 for i in range(4):
